5c6466ac8399b70bf14028773dc612aafac60342
ngokcimen <nilgokcimen@gmail.com>
2019-02-26 21:03:38 +0300

    mqContains test added

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -30,4 +30,14 @@ public class TestCompute {
     when(c.mq.size()).thenReturn(3);
     assertEquals(0, c.countNumberOfOccurrences(""));
   }
+
+  @Test
+  public void mqContains() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    String e = "1";
+    when(c.mq.size()).thenReturn(3);
+    when(c.mq.contains(e)).thenReturn(true);
+    assertEquals(0, c.countNumberOfOccurrences(e));
+  }
 }
