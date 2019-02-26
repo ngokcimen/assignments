aa418430bec47f68779431765362b69fbc2fe948
ngokcimen <nilgokcimen@gmail.com>
2019-02-26 21:02:41 +0300

    mqNotZero test added

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -22,4 +22,12 @@ public class TestCompute {
     when(c.mq.size()).thenReturn(0);
     assertEquals(-1, c.countNumberOfOccurrences(""));
   }
+
+  @Test
+  public void mqNotZero() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(c.mq.size()).thenReturn(3);
+    assertEquals(0, c.countNumberOfOccurrences(""));
+  }
 }
